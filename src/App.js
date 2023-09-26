580f8115f4cb167b0fc418421943b391edee1ea9
Neeraj <neeraj.chaudhary_cs19@gla.ac.in>
2023-09-26 06:26:40 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ import RecipeTile from "./RecipeTile";
 function App() {
   const [query, setQuery] = useState("");
   const [recipes, setRecipes] = useState([]);
+  const [healthLabels, setHealthLabels] = useState('vegan')
   
-  const url = `https://api.edamam.com/search?q=${query}&app_id=badfa13a&app_key=67b4a79d44c512610e649442b30002b8`;
+  const url = `https://api.edamam.com/search?q=${query}&app_id=badfa13a&app_key=67b4a79d44c512610e649442b30002b8&health=${healthLabels}`;
 
   async function getRecipesInfo() {
     var result = await Axios.get(url);
@@ -33,6 +34,10 @@ function App() {
      value={query} onChange={(e) => setQuery(e.target.value) }
      />
      <input className="Submit" type="Submit" value="Search" />
+
+     <select className="healthLabels">
+      <option onClick={() => setHealthLabels('vegan')}>Vegan</option>
+     </select>
    </form>
 
    <div className="Recipes">
